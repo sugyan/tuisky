ccc768a1b9c9ddd896631a613304ae71ba33a803
sugyan <sugi1982@gmail.com>
2024-07-20 23:38:59 +0900

    Update login view, remove NextInput/PrevInput action

--- src/components/views/login.rs
+++ src/components/views/login.rs
@@ -2,49 +2,99 @@ use super::types::{Action, View};
 use super::ViewComponent;
 use bsky_sdk::BskyAgent;
 use color_eyre::Result;
-use crossterm::event::KeyEvent;
+use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 use ratatui::layout::{Constraint, Layout, Rect};
-use ratatui::style::{Color, Style, Stylize};
-use ratatui::widgets::{Block, Borders, Padding, Paragraph, Wrap};
+use ratatui::style::{Style, Stylize};
+use ratatui::text::Line;
+use ratatui::widgets::{Block, Padding, Paragraph, Wrap};
 use ratatui::Frame;
 use std::sync::{Arc, RwLock};
 use tokio::sync::mpsc::UnboundedSender;
-use tui_prompts::{State, TextPrompt, TextRenderStyle, TextState};
+use tui_textarea::TextArea;
 
-enum FocusField {
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Focus {
     Identifier,
     Password,
-    None,
+    Submit,
+}
+
+impl Focus {
+    fn next(&self) -> Self {
+        match self {
+            Self::Identifier => Self::Password,
+            Self::Password => Self::Submit,
+            Self::Submit => Self::Identifier,
+        }
+    }
+    fn prev(&self) -> Self {
+        match self {
+            Self::Identifier => Self::Submit,
+            Self::Password => Self::Identifier,
+            Self::Submit => Self::Password,
+        }
+    }
 }
 
 pub struct LoginComponent {
-    identifier: TextState<'static>,
-    password: TextState<'static>,
-    current: FocusField,
+    identifier: TextArea<'static>,
+    password: TextArea<'static>,
+    focus: Focus,
     error_message: Arc<RwLock<Option<String>>>,
     action_tx: UnboundedSender<Action>,
 }
 
 impl LoginComponent {
     pub fn new(action_tx: UnboundedSender<Action>) -> Self {
+        let mut identifier = TextArea::default();
+        identifier.set_block(
+            Block::bordered()
+                .title("Identifier")
+                .border_style(Style::default()),
+        );
+        identifier.set_cursor_line_style(Style::default());
+        let mut password = TextArea::default();
+        password.set_mask_char('*');
+        password.set_block(
+            Block::bordered()
+                .title("Password")
+                .border_style(Style::default().dim()),
+        );
+        password.set_cursor_line_style(Style::default());
+        password.set_cursor_style(Style::default());
         Self {
-            identifier: TextState::new(),
-            password: TextState::new(),
-            current: FocusField::Identifier,
+            identifier,
+            password,
+            focus: Focus::Identifier,
             error_message: Arc::new(RwLock::new(None)),
             action_tx,
         }
     }
-    fn current(&mut self) -> Option<&mut TextState<'static>> {
-        match self.current {
-            FocusField::Identifier => Some(&mut self.identifier),
-            FocusField::Password => Some(&mut self.password),
-            FocusField::None => None,
+    fn current_textarea(&mut self) -> Option<&mut TextArea<'static>> {
+        match self.focus {
+            Focus::Identifier => Some(&mut self.identifier),
+            Focus::Password => Some(&mut self.password),
+            Focus::Submit => None,
+        }
+    }
+    fn update_focus(&mut self, focus: Focus) {
+        if let Some(textarea) = self.current_textarea() {
+            textarea.set_cursor_style(Style::default());
+            if let Some(block) = textarea.block().cloned() {
+                textarea.set_block(block.border_style(Style::default().dim()));
+            }
+        }
+        self.focus = focus;
+        if let Some(textarea) = self.current_textarea() {
+            textarea.set_cursor_style(Style::default().reversed());
+            if let Some(block) = textarea.block().cloned() {
+                textarea.set_block(block.border_style(Style::default()));
+            }
         }
     }
     fn login(&self) -> Result<()> {
-        let identifier = self.identifier.value().to_string();
-        let password = self.password.value().to_string();
+        let identifier = self.identifier.lines().join("");
+        let password = self.password.lines().join("");
         let error_message = Arc::clone(&self.error_message);
         let action_tx = self.action_tx.clone();
         tokio::spawn(async move {
@@ -78,62 +128,64 @@ impl ViewComponent for LoginComponent {
         View::Login
     }
     fn handle_key_events(&mut self, key: KeyEvent) -> Result<Option<Action>> {
-        if let Some(current) = self.current() {
-            current.handle_key_event(key);
-            if let Err(e) = self.action_tx.send(Action::Render) {
-                log::error!("failed to send render event: {e}");
-            }
+        if let Some(textarea) = self.current_textarea() {
+            Ok(match (key.code, key.modifiers) {
+                (KeyCode::Enter, _) | (KeyCode::Char('m'), KeyModifiers::CONTROL) => {
+                    Some(Action::Enter)
+                }
+                _ => {
+                    if textarea.input(key) {
+                        Some(Action::Render)
+                    } else {
+                        None
+                    }
+                }
+            })
+        } else {
+            Ok(None)
         }
-        Ok(None)
     }
     fn update(&mut self, action: Action) -> Result<Option<Action>> {
         match action {
-            Action::NextInput | Action::PrevInput => {
-                self.current = match self.current {
-                    FocusField::Identifier => FocusField::Password,
-                    FocusField::Password => FocusField::Identifier,
-                    FocusField::None => FocusField::Identifier,
-                };
-                return Ok(Some(Action::Render));
+            Action::NextItem => {
+                self.update_focus(self.focus.next());
+                Ok(Some(Action::Render))
+            }
+            Action::PrevItem => {
+                self.update_focus(self.focus.prev());
+                Ok(Some(Action::Render))
             }
             Action::Enter => {
-                if self.identifier.is_finished() && self.password.is_finished() {
-                    self.current = FocusField::None;
-                    self.login()?;
-                } else {
-                    return Ok(Some(Action::NextInput));
+                match self.focus {
+                    Focus::Submit => {
+                        self.login()?;
+                    }
+                    _ => {
+                        self.update_focus(self.focus.next());
+                    }
                 }
-                return Ok(Some(Action::Render));
+                Ok(Some(Action::Render))
             }
-            _ => {}
+            _ => Ok(None),
         }
-        Ok(None)
     }
     fn draw(&mut self, f: &mut Frame<'_>, area: Rect) -> Result<()> {
         let block = Block::default().padding(Padding::proportional(2));
-        let block_padding = Block::default().padding(Padding::bottom(1));
-        let block_border = Block::default()
-            .borders(Borders::BOTTOM)
-            .border_style(Style::default().fg(Color::Gray).dim());
         let layout = Layout::vertical([
-            Constraint::Length(2),
+            Constraint::Length(3),
+            Constraint::Length(3),
             Constraint::Length(1),
-            Constraint::Length(2),
             Constraint::Length(1),
             Constraint::Length(2),
         ])
         .split(block.inner(area));
-        let mut identifier = TextPrompt::from("Identifier").with_block(block_padding.clone());
-        let mut password = TextPrompt::from("Password  ")
-            .with_render_style(TextRenderStyle::Password)
-            .with_block(block_padding.clone());
-        match self.current {
-            FocusField::Identifier => identifier = identifier.with_block(block_border.clone()),
-            FocusField::Password => password = password.with_block(block_border.clone()),
-            _ => {}
+        let mut submit = Line::from("Submit").blue().centered();
+        if self.focus == Focus::Submit {
+            submit = submit.reversed();
         }
-        f.render_stateful_widget(identifier, layout[0], &mut self.identifier);
-        f.render_stateful_widget(password, layout[2], &mut self.password);
+        f.render_widget(self.identifier.widget(), layout[0]);
+        f.render_widget(self.password.widget(), layout[1]);
+        f.render_widget(submit, layout[2]);
         if let Ok(message) = self.error_message.read() {
             if let Some(s) = message.as_ref() {
                 f.render_widget(
